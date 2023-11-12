fbd8e9ec2a6dac357b2a61c9762805de6b45dbab
Manfred Lotz <manfred.lotz@posteo.de>
2023-11-12 17:08:53 +0100

    switch from run_script to duct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::fs::{File, OpenOptions};
-use std::io::{Error, Write};
+use std::io::Error;
 use std::path::PathBuf;
+use std::process;
 
 use clap::{Parser, ValueHint};
-use run_script::ScriptOptions;
+
+use duct::cmd;
 
 #[derive(Parser, Debug, PartialEq)]
 #[clap(author, version, about, long_about = None)]
@@ -15,51 +16,92 @@ struct Cli {
     #[clap(short = 'c', long = "command", help = "Command to execute", value_hint = ValueHint::FilePath)]
     command: Option<String>,
 
-    #[clap(short = 'e', long = "stderr", help = "Trigger output when stderr is not empty")]
+    #[clap(
+        short = 'e',
+        long = "stderr",
+        help = "Trigger output when stderr is not empty"
+    )]
     stderr: bool,
 }
 
-fn write_logfile(logfile_path: Option<PathBuf>, stdout: &str, stderr: &str) -> Result<(), Error> {
-    if let Some(logfile) = logfile_path.as_deref() {
-
-        // Open log file with append option or create if not existing
-        let mut f = if logfile.exists() {
-            OpenOptions::new()
-                .append(true)
-                .open(logfile)
-                .unwrap_or_else(|_| panic!("Cannot open logfile `{}`", logfile.display()))
-        } else {
-            File::create(logfile).unwrap_or_else(|_| panic!("Unable to create logfile `{}`", logfile.display()))
-        };
-
-        write!(f, "{}", stdout)?;
-        write!(f, "{}", stderr)?;
+fn print_stdout(output: &[u8]) {
+    for line in String::from_utf8_lossy(output).lines() {
+        log::info!("{}", line);
     }
-    Ok(())
 }
 
-fn print_errors(code: i32, output: &str , error: &str) {
-    println!("Exit Code: {}", code);
-    println!("STDOUT\n{}", output);
-    println!("STDERR\n{}", error);
+fn print_stderr(output: &[u8]) {
+    for line in String::from_utf8_lossy(output).lines() {
+        log::error!("{}", line);
+    }
 }
 
 fn main() -> Result<(), Error> {
     let cli = Cli::parse();
-
-    let options = ScriptOptions::new();
-    let args = vec![];
+    if let Some(logfile) = cli.logfile_path {
+        // Apply globally
+        fern::Dispatch::new()
+            // Perform allocation-free log formatting
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "[{} {:5}] {}",
+                    humantime::format_rfc3339(std::time::SystemTime::now()),
+                    record.level(),
+                    //                record.target(),
+                    message
+                ))
+            })
+            // Add blanket level filter -
+            .level(log::LevelFilter::Debug)
+            // - and per-module overrides
+            .level_for("rcronic", log::LevelFilter::Info)
+            // Output to stdout, files, and other Dispatch configurations
+            .chain(std::io::stdout())
+            .chain(fern::log_file(logfile)?)
+            .apply()
+            .unwrap();
+    } else {
+        fern::Dispatch::new()
+            // Perform allocation-free log formatting
+            .format(|out, message, record| {
+                out.finish(format_args!(
+                    "[{} {:5}] {}",
+                    humantime::format_rfc3339(std::time::SystemTime::now()),
+                    record.level(),
+                    //                record.target(),
+                    message
+                ))
+            })
+            // Add blanket level filter -
+            .level(log::LevelFilter::Debug)
+            // - and per-module overrides
+            .level_for("rcronic", log::LevelFilter::Info)
+            // Output to stdout, files, and other Dispatch configurations
+            .chain(std::io::stdout())
+            .apply()
+            .unwrap();
+    };
 
     if let Some(command) = cli.command {
-        let (code, output, error) = run_script::run(&command, &args, &options).unwrap();
+        let output_obj = cmd!("sh", "-c", &command)
+            .stderr_capture()
+            .stdout_capture()
+            .unchecked()
+            .run()?;
 
-        write_logfile(cli.logfile_path, &output, &error)?;
-        if code != 0 {
-            println!("cronic: detected error in command `{}`", command);
-            print_errors(code, &output, &error);
-        } else if cli.stderr && !error.is_empty() {
-            println!("cronic: detected error output in command `{}`", command);
-            print_errors(code, &output, &error);
+        if !&output_obj.status.success() || (!output_obj.stderr.is_empty() && cli.stderr) {
+            log::info!("rcronic detected error in command `{}`", &command);
+            print_stdout(&output_obj.stdout);
+            print_stderr(&output_obj.stderr);
+            if output_obj.status.success() {
+                log::info!("Return code: {}", &output_obj.status);
+            } else {
+                log::error!("Return code: {}", &output_obj.status);
+                process::exit(1);
+            }
+        // } else {
+        //     log::info!("Return code: {}", &output_obj.status);
+        //
         }
     }
 
