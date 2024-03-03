7b87f7b77fd7c1422659f6bc169bca33a2c7ee34
Manfred Lotz <manfred.lotz@posteo.de>
2024-03-03 13:41:31 +0100

    - now using simplelog instead of fern - correct errors when in some situations the writing behavior was wrong

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use std::fs::OpenOptions;
 use std::io::Error;
 use std::path::PathBuf;
-use std::process;
 
 use clap::{Parser, ValueHint};
-
 use duct::cmd;
+use simplelog::*;
 
 #[derive(Parser, Debug, PartialEq)]
 #[clap(author, version, about, long_about = None)]
@@ -14,7 +14,7 @@ struct Cli {
     logfile_path: Option<PathBuf>,
 
     #[clap(short = 'c', long = "command", help = "Command to execute", value_hint = ValueHint::FilePath)]
-    command: Option<String>,
+    command: String,
 
     #[clap(
         short = 'e',
@@ -36,74 +36,248 @@ fn print_stderr(output: &[u8]) {
     }
 }
 
+// fn setup_logging(logfile_path: Option<PathBuf>) {
+//
+//     if let Some(logfile) = logfile_path {
+//
+//         let file = OpenOptions::new().write(true).append(true).create(true).open(logfile).expect("Cannot open log file");
+//         CombinedLogger::init(vec![
+//             TermLogger::new(
+//                 LevelFilter::Info,
+//                 config.clone(),
+//                 TerminalMode::Mixed,
+//                 ColorChoice::Auto,
+//             ),
+//             WriteLogger::new(
+//                 LevelFilter::Info,
+//                 config.clone(),
+//                 //File::create(logfile).unwrap(),
+//                 file,
+//             ),
+//         ])
+//         .unwrap();
+//     } else {
+//         TermLogger::init(
+//             LevelFilter::Warn,
+//             config,
+//             TerminalMode::Mixed,
+//             ColorChoice::Auto,
+//         )
+//         .unwrap();
+//     };
+//
+// }
+//
+
+// fn get_logger(term_logger: Option<&TermLogger>, write_logger: Option<&WriteLogger<&str>>) -> Result<(), SetLoggerError> {
+//     let mut loggers: Vec<Box<dyn simplelog::SharedLogger>> = Vec::new();
+//
+//     if let Some(term_logger_conf) = term_logger {
+//         loggers.push(term_logger_conf)
+//     }
+//     if let Some(write_logger_conf) = write_logger {
+//         loggers.push(write_logger_conf)
+//     }
+//
+//     CombinedLogger::init(loggers)
+// }
+
+// fn get_terminal_logger(config: simplelog::Config ) -> Box<simplelog::TermLogger> {
+//     TermLogger::new(
+//         LevelFilter::Info,
+//         config.clone(),
+//         TerminalMode::Mixed,
+//         ColorChoice::Auto,
+//     )
+// }
+
+// fn get_write_logger(config: simplelog::Config, logfile: &PathBuf  ) -> Box<simplelog::WriteLogger> {
+//
+//     let file = OpenOptions::new().write(true).append(true).create(true).open(logfile).expect("Cannot open log file");
+//     WriteLogger::new(
+//         LevelFilter::Info,
+//         config.clone(),
+//         //File::create(logfile).unwrap(),
+//         file,
+//     )
+// }
 fn main() -> Result<(), Error> {
     let cli = Cli::parse();
-    if let Some(logfile) = cli.logfile_path {
-        // Apply globally
-        fern::Dispatch::new()
-            // Perform allocation-free log formatting
-            .format(|out, message, record| {
-                out.finish(format_args!(
-                    "[{} {:5}] {}",
-                    humantime::format_rfc3339(std::time::SystemTime::now()),
-                    record.level(),
-                    //                record.target(),
-                    message
-                ))
-            })
-            // Add blanket level filter -
-            .level(log::LevelFilter::Debug)
-            // - and per-module overrides
-            .level_for("rcronic", log::LevelFilter::Info)
-            // Output to stdout, files, and other Dispatch configurations
-            .chain(std::io::stdout())
-            .chain(fern::log_file(logfile)?)
-            .apply()
-            .unwrap();
-    } else {
-        fern::Dispatch::new()
-            // Perform allocation-free log formatting
-            .format(|out, message, record| {
-                out.finish(format_args!(
-                    "[{} {:5}] {}",
-                    humantime::format_rfc3339(std::time::SystemTime::now()),
-                    record.level(),
-                    //                record.target(),
-                    message
-                ))
-            })
-            // Add blanket level filter -
-            .level(log::LevelFilter::Debug)
-            // - and per-module overrides
-            .level_for("rcronic", log::LevelFilter::Info)
-            // Output to stdout, files, and other Dispatch configurations
-            .chain(std::io::stdout())
-            .apply()
-            .unwrap();
-    };
 
-    if let Some(command) = cli.command {
-        let output_obj = cmd!("sh", "-c", &command)
-            .stderr_capture()
-            .stdout_capture()
-            .unchecked()
-            .run()?;
+    let config = ConfigBuilder::new()
+        .set_level_color(Level::Error, Some(Color::Magenta))
+        .set_level_color(Level::Trace, Some(Color::Green))
+        .set_time_format_rfc3339()
+        .set_time_offset_to_local()
+        .unwrap()
+        .build();
 
-        if !&output_obj.status.success() || (!output_obj.stderr.is_empty() && cli.stderr) {
-            log::info!("rcronic detected error in command `{}`", &command);
-            print_stdout(&output_obj.stdout);
-            print_stderr(&output_obj.stderr);
-            if output_obj.status.success() {
-                log::info!("Return code: {}", &output_obj.status);
-            } else {
-                log::error!("Return code: {}", &output_obj.status);
-                process::exit(1);
-            }
-        // } else {
-        //     log::info!("Return code: {}", &output_obj.status);
-        //
+    let output_obj = cmd!("sh", "-c", &cli.command)
+        .stderr_capture()
+        .stdout_capture()
+        .unchecked()
+        .run()?;
+
+    // - command had error
+    // - or there is stderr output and we requested -e
+    // Then we want output to terminal and to logger if logging was requested
+    match (
+        &output_obj.status.success(),
+        cli.logfile_path,
+        cli.stderr,
+        output_obj.stderr.is_empty(),
+    ) {
+        (true, None, false, _) => (), //println!("print no output at all"),
+
+        (false, Some(logfile), _, _) | (true, Some(logfile), true, _) => {
+            //println!("output stderr/stdout to terminal and logfile");
+            log::info!("rcronic detected error in command `{}`", &cli.command);
+            let file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(logfile)
+                .expect("Cannot open log file");
+            CombinedLogger::init(vec![
+                TermLogger::new(
+                    LevelFilter::Info,
+                    config.clone(),
+                    TerminalMode::Mixed,
+                    ColorChoice::Auto,
+                ),
+                WriteLogger::new(
+                    LevelFilter::Info,
+                    config.clone(),
+                    //File::create(logfile).unwrap(),
+                    file,
+                ),
+            ])
+            .unwrap();
+        }
+
+        (true, None, true, _) | (false, None, _, _) => {
+            //println!("output stderr/stdour to terminal");
+
+            TermLogger::init(
+                LevelFilter::Warn,
+                config,
+                TerminalMode::Mixed,
+                ColorChoice::Auto,
+            )
+            .unwrap();
+        }
+        (true, Some(logfile), false, _) => {
+            //println!("output stderr/stdour to logfile");
+            let file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(logfile)
+                .expect("Cannot open log file");
+            WriteLogger::init(
+                LevelFilter::Info,
+                config,
+                //File::create(logfile).unwrap(),
+                file,
+            )
+            .unwrap();
         }
     }
+
+    print_stdout(&output_obj.stdout);
+    print_stderr(&output_obj.stderr);
+    //process::exit(42);
+    // if !&output_obj.status.success() {
+    //     if let Some(logfile) = cli.logfile_path {
+    //        // both logfile and stderr /stdout
+    //         CombinedLogger::init(vec![term_logger, write_logger]);
+    //     } else {
+    //        // only stderr and stdout
+    //         CombinedLogger::init(vec![term_logger]);
+    //     }
+    // } else {
+    //     (!output_obj.stderr.is_empty() && cli.stderr) {
+    //
+    // }
+    //
+    //
+
+    //         if !&output_obj.status.success()  {
+    //             println!("output to stdout/stderr and if chosen the logfile");
+    //             if let Some(logfile) = cli.logfile_path {
+    //                 println!("output to logfile and stderr");
+    // //                setup_logging(&logfile);
+    //             }
+    //
+    //             log::info!("rcronic detected error in command `{}`", &command);
+    //             print_stdout(&output_obj.stdout);
+    //             print_stderr(&output_obj.stderr);
+    //             log::error!("Return code: {}", &output_obj.status);
+    //         } else {
+    //             if let Some(logfile) = cli.logfile_path {
+    //                 println!("output to logfile only");
+    //             };
+    //
+    //         }
+
+    // let config = ConfigBuilder::new()
+    //     .set_level_color(Level::Error, Some(Color::Magenta))
+    //     .set_level_color(Level::Trace, Some(Color::Green))
+    //     .set_time_format_rfc3339()
+    //     .set_time_offset_to_local()
+    //     .unwrap()
+    //     .build();
+    // if let Some(logfile) = cli.logfile_path {
+    //
+    //     let file = OpenOptions::new().write(true).append(true).create(true).open(logfile).expect("Cannot open log file");
+    //     CombinedLogger::init(vec![
+    //         TermLogger::new(
+    //             LevelFilter::Info,
+    //             config.clone(),
+    //             TerminalMode::Mixed,
+    //             ColorChoice::Auto,
+    //         ),
+    //         WriteLogger::new(
+    //             LevelFilter::Info,
+    //             config.clone(),
+    //             //File::create(logfile).unwrap(),
+    //             file,
+    //         ),
+    //     ])
+    //     .unwrap();
+    // } else {
+    //     TermLogger::init(
+    //         LevelFilter::Warn,
+    //         config,
+    //         TerminalMode::Mixed,
+    //         ColorChoice::Auto,
+    //     )
+    //     .unwrap();
+    // };
+
+    // if let Some(command) = cli.command {
+    //     let output_obj = cmd!("sh", "-c", &command)
+    //         .stderr_capture()
+    //         .stdout_capture()
+    //         .unchecked()
+    //         .run()?;
+    //
+    //     if !&output_obj.status.success() || (!output_obj.stderr.is_empty() && cli.stderr) {
+    //         log::info!("rcronic detected error in command `{}`", &command);
+    //         print_stdout(&output_obj.stdout);
+    //         print_stderr(&output_obj.stderr);
+    //         if output_obj.status.success() {
+    //             log::info!("Return code: {}", &output_obj.status);
+    //         } else {
+    //             log::error!("Return code: {}", &output_obj.status);
+    //             process::exit(1);
+    //         }
+    //         // } else {
+    //         //     log::info!("Return code: {}", &output_obj.status);
+    //         //
+    //     } else {
+    //
+    //         print_stdout(&output_obj.stdout);
+    //     }
+    // }
 
     Ok(())
 }
